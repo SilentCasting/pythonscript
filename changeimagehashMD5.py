32db9ac22e51f02da29e624ab276c2b9bf581e98
Ihor <66572758+SilentCasting@users.noreply.github.com>
2022-02-21 12:50:36 +0200

    tk
    Tk work

--- changeimagehashMD5.py
+++ changeimagehashMD5.py
@@ -1,14 +1,28 @@
+import tkinter as tk
 import hashlib, random
-L = str(random.randint(8, 888))
-md5_hash = hashlib.md5()
-a_file = open("image.jpg", "ab")
-for item in L:
-    s = str(item) + '\n'
-    bt = s.encode()
-    a_file.write(bt)
-a_file.close()
-a_file = open("image.jpg", "rb")
-content = a_file.read()
-md5_hash.update(content)
-digest = md5_hash.hexdigest()
-print(digest)
+import tkinter.filedialog
+root = tkinter.Tk()
+def print_path():
+    fimage = tkinter.filedialog.askopenfilename(
+        parent=root, initialdir='C:/Tutorial',
+        title='Choose file',
+        filetypes=[('png images', '.png'),
+                   ('gif images', '.gif')]
+        )
+    print(fimage)
+    L = str(random.randint(8, 888))
+    md5_hash = hashlib.md5()
+    a_file = open(fimage, "ab")
+    for item in L:
+     s = str(item) + '\n'
+     bt = s.encode()
+     a_file.write(bt)
+    a_file.close()
+    a_file = open(fimage, "rb")
+    content = a_file.read()
+    md5_hash.update(content)
+    digest = md5_hash.hexdigest()
+    print(digest)
+b1 = tkinter.Button(root, text='Choose file(and random md5 hash)', command=print_path)
+b1.pack(fill='x')
+root.mainloop()
